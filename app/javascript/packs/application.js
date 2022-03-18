d559782e7567cc797577d35de9262c5f6e9df09c
bd00051 <bd00051@cs6242-vm01.uwg.westga.edu>
2022-03-18 17:31:39 -0400

    installed Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,23 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//Bootstrap loader
+require("@popperjs/core")
+import "bootstrap"
+import { Tooltip, Popover } from "bootstrap"
+require("../stylesheets/application.scss")
+document.addEventListener("turbolinks:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+    return new Popover(popoverTriggerEl)
+  })
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
