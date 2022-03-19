da85db4943628ffde819a354581cf7cb62faed82
bd00051 <bd00051@cs6242-vm02.uwg.westga.edu>
2022-03-19 12:31:48 -0400

    set default values of votes to 0, added validation to prevent negative numbers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -65,6 +65,8 @@ class RestaurantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :location, :will_split, :wont_split)
+      params
+        .require(:restaurant)
+        .permit(:name, :location, :will_split, :wont_split)
     end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,16 @@
 class Restaurant < ApplicationRecord
+  after_initialize :set_defaults, unless: :persisted?
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   validates :location, presence: true, format: {
       with: /\A[a-zA-Z\s]+,\s[A-Z]{2}\z/,
       message: "Location must be of format City, ST"
     }
+  validates :will_split, :wont_split,
+    numericality: { greater_than_or_equal_to: 0 }
+
+  def set_defaults
+    self.will_split ||= 0
+    self.wont_split ||= 0
+  end
 end
