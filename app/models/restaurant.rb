5c1c9770800653c4aa2ed634f1baa99022a06d71
bd00051 <bd00051@cs6242-vm02.uwg.westga.edu>
2022-03-19 13:42:45 -0400

    designed website banner and changed error message style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,11 @@
 class Restaurant < ApplicationRecord
   after_initialize :set_defaults, unless: :persisted?
   default_scope -> { order(created_at: :desc) }
+  #Parameter validation
   validates :name, presence: true
   validates :location, presence: true, format: {
       with: /\A[a-zA-Z\s]+,\s[A-Z]{2}\z/,
-      message: "Location must be of format City, ST"
+      message: "must be of format City, ST"
     }
   validates :will_split, :wont_split,
     numericality: { greater_than_or_equal_to: 0 }
