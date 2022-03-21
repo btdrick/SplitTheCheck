03ebdfd9fbde069cb6caef63640c8c839f64f6ca
bd00051 <bd00051@cs6242-vm02.uwg.westga.edu>
2022-03-21 18:28:40 -0400

    altered index table header to reflect number of search results

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -2,8 +2,15 @@ module RestaurantsHelper
 
   def create_header
     header = 'Restaurant Listings'
-    if @q.result.count < Restaurant.count
-      header = 'Search Results'
+    if @q.result.count == 0
+      header = 'No Results'
+    elsif @q.result.count < Restaurant.count
+      header = @q.result.count.to_s
+      if @q.result.count == 1
+       header += ' Result Found'
+     else
+       header += ' Results Found'
+     end
     end
     header
   end
