9f7da7308a1cc032a1bc763e5e0c228b650e705c
bd00051 <bd00051@cs6242-vm09.uwg.westga.edu>
2022-04-07 15:00:00 -0400

    added user login, registration, sign out, edit, delete account functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    confirmations: 'users/confirmations',
     sessions: 'users/sessions'
   }
-  
+  devise_scope :users do
+    get 'sign_up', to: 'users/registrations#new'
+    get 'sign_in', to: 'users/sessions#new'
+    delete 'sign_out', to: 'users/sessions#destroy'
+  end
+
   root 'restaurants#index'
   put '/restaurant/:id/vote', to: 'restaurants#vote', as: 'vote'
+  resources :users
   resources :restaurants
 end
