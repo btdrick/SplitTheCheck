e13e640bf193d61f686e56f12a5111cefcc5bea7
bd00051 <bd00051@cs6242-vm04.uwg.westga.edu>
2022-04-21 17:20:34 -0400

    added tests for Favorite

---
+++ test/controllers/favorites_controller_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
+include Devise::Test::IntegrationHelpers
+
+  setup do
+    @favorite = favorites(:one)
+    @restaurant = restaurants(:one)
+    sign_in @favorite.user
+  end
+
+  test "should create favorite" do
+    assert_difference('Favorite.count') do
+      post favorites_url, params: { favorite:
+        { restaurant_id: @restaurant.id } }
+    end
+
+    assert_redirected_to restaurant_url(@restaurant)
+  end
+
+  test "should destroy favorite" do
+    puts favorite_url(@favorite)
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@favorite)
+    end
+
+    assert_redirected_to restaurant_url(@favorite.restaurant)
+  end
+
+end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,5 +1,15 @@
 require "test_helper"
 
 class RestaurantTest < ActiveSupport::TestCase
-  
+  setup do
+    @restaurant = restaurants(:one)
+  end
+
+  test "should get will_split rating" do
+    assert_equal 1, @restaurant.get_will_split_vote_count
+  end
+
+  test "should get wont_split rating" do
+    assert_equal 0, @restaurant.get_wont_split_vote_count
+  end
 end
