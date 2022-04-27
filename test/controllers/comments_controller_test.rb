15e4d7dbebedb4da1872062baf8b61b48cb6f61c
bd00051 <bd00051@cs6242-vm16.uwg.westga.edu>
2022-04-27 16:33:58 -0400

    added testing for Comment controller

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+include Devise::Test::IntegrationHelpers
+
+  setup do
+    @comment = comments(:one)
+    @restaurant = restaurants(:one)
+    sign_in @comment.user
+  end
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post restaurant_comments_path(@restaurant), params:
+      { comment:
+        { restaurant_id: @restaurant.id,
+          content: @comment.content } }
+    end
+
+    assert_redirected_to restaurant_url(@restaurant)
+  end
+  
 end
